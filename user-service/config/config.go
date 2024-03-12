a722320572747e2ecfaa0f21a5cc31c1f58319f1
keldimurodov <keldimurodovsardor@gmail.com>
2024-03-12 21:12:02 +0500

    First added user-service and api-gateway

---
+++ user-service/config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"github.com/spf13/cast"
+	"os"
+)
+
+type Config struct {
+	Envoriment       string //developing, staging, production
+	PostgresHost     string
+	PostgresPort     int
+	PostgresDatabase string
+	PostgresUser     string
+	PostgresPassword string
+	LogLevel         string
+	RPCPort          string
+}
+
+func Load() Config {
+	c := Config{}
+
+	c.Envoriment = cast.ToString(getOrReturnDefault("ENVIROMENT", "develop"))
+	c.PostgresHost = cast.ToString(getOrReturnDefault("POSTGRES_HOST", "localhost"))
+	c.PostgresPort = cast.ToInt(getOrReturnDefault("POSTGRES_PORT", 5432))
+	c.PostgresDatabase = cast.ToString(getOrReturnDefault("POSTGRES_DATABASE", "users"))
+	c.PostgresUser = cast.ToString(getOrReturnDefault("POSTGRES_USER", "postgres"))
+	c.PostgresPassword = cast.ToString(getOrReturnDefault("POSTGRES_PASSWORD", "123"))
+
+	c.LogLevel = cast.ToString(getOrReturnDefault("LOG_LEVEL", "debug"))
+
+	c.RPCPort = cast.ToString(getOrReturnDefault("RPC_PORT", ":9000"))
+
+	return c
+}
+
+func getOrReturnDefault(key string, defaultValue interface{}) interface{} {
+	_, exists := os.LookupEnv(key)
+	if exists {
+		return os.Getenv(key)
+	}
+
+	return defaultValue
+}
---
+++ user-service/go.mod
@@ -0,0 +1,12 @@
+module article/user-service
+
+go 1.22.1
+
+require (
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/lib/pq v1.10.9
+	github.com/spf13/cast v1.6.0
+	go.uber.org/zap v1.27.0
+)
+
+require go.uber.org/multierr v1.10.0 // indirect
